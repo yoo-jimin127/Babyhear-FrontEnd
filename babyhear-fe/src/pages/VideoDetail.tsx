8d743dfa7ba5fe6628b8f6d6686489b6d1c5319b
yoo-jimin127 <dbwlals9936@naver.com>
2023-06-05 16:11:27 +0900

    feat: VideoDetail 페이지 로직 완료

--- babyhear-fe/src/pages/VideoDetail.tsx
+++ babyhear-fe/src/pages/VideoDetail.tsx
@@ -1,9 +1,157 @@
+import { useEffect, useState } from "react";
+import { useParams, useLocation } from "react-router-dom";
+import styled from "styled-components";
+import FeatBg from "../assets/featBg.png";
+import LogoImg from "../assets/logo.png";
+import YoutubeThumbnail from "../components/YoutubeThumbnail";
+import { VIDEO_URLS } from '../static/videoUrl';
+
 const VideoDetail = () => {
+  const videoId = useParams().videoId;
+  const location = useLocation();
+  const { category } = location.state || {};
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const IntVideoId = parseInt(videoId || "0", 10) as number;
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
-    <>
-    VideoDetail
-    </>
+    <Container>
+      <TopWrapper  windowWidth={windowWidth}>
+        <TitleBox>
+          <Logo src={LogoImg} alt='logo-image' />
+          <Title>
+            베이비 히어에서 제공하는<br />
+            다양한 서비스를 사용해보세요.
+          </Title>
+        </TitleBox>
+      </TopWrapper>
+      <BottomWrapper>
+        <CategoryTitleBox>
+          <VideoCategory>{category}</VideoCategory>
+          <SubTitle>시청을 희망하시는 영상을 클릭해주세요!</SubTitle>
+        </CategoryTitleBox>
+        <VideoTitle>
+          <span style={{ color: "var(--highlight)"}}>아이</span>가 좋아할 영상이에요.
+        </VideoTitle>
+        <ContentBox>
+          {VIDEO_URLS[IntVideoId].slice(0, 6).map((url, index) => (
+            <YoutubeThumbnail key={index} videoUrl={url} />
+          ))}
+        </ContentBox>
+        <VideoTitle>
+          <span style={{ color: "var(--highlight)"}}>부모님</span>이 좋아하실 영상이에요.
+        </VideoTitle>
+        <ContentBox>
+          {VIDEO_URLS[IntVideoId].slice(6, 12).map((url, index) => (
+            <YoutubeThumbnail key={index} videoUrl={url} />
+          ))}
+        </ContentBox>
+      </BottomWrapper>
+    </Container>
   );
 }
 
 export default VideoDetail;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const TopWrapper = styled.div<{ windowWidth: number }>`
+  width: ${(props) => (props.windowWidth < 390 ? "100%" : "390px" )};
+  height: 100px;
+  background-image: url(${FeatBg});
+  background-position: top;
+  background-size: cover;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+`;
+
+const TitleBox = styled.div`
+  display: flex;
+  align-items: flex-start;
+  margin: 10px 0 0 10px;
+  margin-right: auto;
+`;
+
+const Logo = styled.img`
+  width: 60px;
+`;
+
+const Title = styled.div`
+  color: #ffffff;
+  font-size: 15px;
+  font-weight: bold;
+  line-height: 1.7;
+`;
+
+const SubTitle = styled.div`
+  color: var(--sub-text);
+  font-size: 13px;
+  margin: -10px 0 20px 20px;
+  margin-right: auto;
+`;
+
+const BottomWrapper = styled.div`
+  height: 650px;
+  display: flex;
+  margin-top: 50px;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+
+  @media (max-width: 420px) {
+    justify-content: space-around;
+  }
+`;
+
+const VideoCategory = styled.div`
+  color: var(--text-default);
+  font-size: 20px;
+  font-weight: bold;
+  margin: -20px 0 20px 20px;
+  margin-right: auto;
+`;
+
+const CategoryTitleBox = styled.div`
+  width: 390px;
+  height: 40px;
+  display: flex;
+  flex-direction: column;
+  justify-items: flex-start;
+`;
+
+const ContentBox = styled.div`
+  width: 380px;
+  height: 300px;
+  max-height: 320px;
+  overflow-y: scroll;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: space-evenly;
+`;
+
+const VideoTitle = styled.div`
+  width: 370px;
+  color: var(--sub-text);
+  font-size: 15px;
+  font-weight: bold;
+  margin: 40px 0 0 10px;
+`;
