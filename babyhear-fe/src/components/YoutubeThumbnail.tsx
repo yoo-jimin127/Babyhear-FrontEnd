288aa191f417f5932acbccb2b73eb3e6f14669fb
yoo-jimin127 <dbwlals9936@naver.com>
2023-06-05 15:03:55 +0900

    chore: 유튜브 API 연결

--- babyhear-fe/src/components/YoutubeThumbnail.tsx
+++ babyhear-fe/src/components/YoutubeThumbnail.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import YouTube from 'react-youtube';
+import styled from "styled-components";
 
 interface YouTubeThumbnailProps {
   videoUrl: string;
@@ -14,7 +15,7 @@ const YouTubeThumbnail: React.FC<YouTubeThumbnailProps> = ({ videoUrl }) => {
       try {
         const videoId = getVideoIdFromUrl(videoUrl);
         const response = await axios.get(
-          `https://www.googleapis.com/youtube/v3/videos?id=${videoId}&key=YOUR_API_KEY&part=snippet`
+          `https://www.googleapis.com/youtube/v3/videos?id=${videoId}&key=${process.env.REACT_APP_YOUTUBE_API_KEY}&part=snippet`
         );
 
         const thumbnailUrl = response.data.items[0].snippet.thumbnails.medium.url;
@@ -33,10 +34,24 @@ const YouTubeThumbnail: React.FC<YouTubeThumbnailProps> = ({ videoUrl }) => {
   };
 
   return thumbnail ? (
-    <img src={thumbnail} alt="YouTube Thumbnail" />
-  ) : (
-    <YouTube videoId={getVideoIdFromUrl(videoUrl) || ''} />
-  );
+    <ThumbnailContainer>
+      <ThumbnailImage src={thumbnail} alt="YouTube Thumbnail" />
+      <YouTube videoId={getVideoIdFromUrl(videoUrl) || ''} />
+    </ThumbnailContainer>
+  ) : null;
 };
 
 export default YouTubeThumbnail;
+
+const ThumbnailContainer = styled.div`
+  width: 170px;
+  height: 95px;
+  border-radius: 10px;
+  overflow: hidden;
+`;
+
+const ThumbnailImage = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`;
